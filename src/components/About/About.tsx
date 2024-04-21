d81770024251e166dac1438598ca6d2a6c2fb79f
Dhruvil Patel <dhruvilpatel2002@gmail.com>
2024-04-21 23:06:06 +0530

    init commit

---
+++ src/components/About/About.tsx
@@ -0,0 +1,93 @@
+import { Container } from "./styles";
+import DhruvilPatel from "../../assets/DhruvilImage.png";
+import htmlIcon from "../../assets/html-icon.svg";
+import cssIcon from "../../assets/css-icon.svg";
+import jsIcon from "../../assets/js-icon.svg";
+import nodeIcon from "../../assets/node-icon.svg";
+import reactIcon from "../../assets/react-icon.svg";
+import pyIcon from "../../assets/pyImage.svg";
+import javaIcon from "../../assets/javaImage.svg";
+import cppIcon from "../../assets/cppImage.svg";
+import ScrollAnimation from "react-animate-on-scroll";
+
+export function About() {
+  return (
+    <Container id="about">
+      <div className="about-text">
+        <ScrollAnimation animateIn="fadeInLeft">
+          <h2>About me</h2>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.1 * 1000}>
+          <p>
+            Hi there! I'm Dhruvil, I'm a skilled automation and backend development professional with a track record of innovative solutions. Currently, I'm an Automation Intern at UBS (Union Bank of Switzerland), where I've created Python-based automation that saves the Valuations Department over 50 hours a month. At CollabNation, I was key to backend development, improving user experience through robust features and integrated analytics.
+          </p>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.2 * 1000} style={{ marginTop: "2rem", marginBottom: "2rem" }}>
+          <p>
+          My personal projects reflect my passion for technology and problem-solving. I've developed a predictive text generator and a farmers' market platform, both highlighting my technical capabilities. I'm proficient in several programming languages and frameworks, with a strong interest in full-stack development, data science, and automation.
+          </p>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.3 * 1000}>
+          <p>
+          As a team player, I value communication and collaboration. My experience with supervisory platforms and data acquisition has further enhanced these skills. I believe my diverse background and collaborative approach make me a valuable asset to any team or organization.
+          </p>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.4 * 1000}>
+          <h3>Here are my main skills:</h3>
+        </ScrollAnimation>
+        <div className="hard-skills">
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.13 * 1000}>
+              <img src={nodeIcon} alt="Node" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.14 * 1000}>
+              <img src={jsIcon} alt="Javascript" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.19 * 1000}>
+              <img src={pyIcon} alt="python" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.19 * 1000}>
+              <img src={javaIcon} alt="python" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.19 * 1000}>
+              <img src={cppIcon} alt="python" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.16 * 1000}>
+              <img src={reactIcon} alt="React" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.17 * 1000}>
+              <img src={htmlIcon} alt="Html" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.18 * 1000}>
+              <img src={cssIcon} alt="Css" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.19 * 1000}>
+              <img src={jsIcon} alt="JavaScript" />
+            </ScrollAnimation>
+          </div>
+        </div>
+      </div>
+      <div className="about-image">
+        <ScrollAnimation animateIn="fadeInRight" delay={0.20 * 1000}>
+          <img src={DhruvilPatel} alt="Dhruvil Patel" />
+        </ScrollAnimation>
+      </div>
+    </Container>
+  )
+}
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,53 @@
+import { Container } from './styles'
+import reactIcon from '../../assets/react-icon.svg'
+import linkedin from '../../assets/linkedin.svg'
+import githubIcon from '../../assets/github.svg'
+import whatsapp from '../../assets/whatsapp.svg'
+import instagramIcon from '../../assets/instagram.svg'
+
+export function Footer() {
+  return (
+    <Container className="footer">
+      <a href="https://dhruvil1808.github.io/resume" className="logo">
+        <span>www.dhruvil</span>
+        <span>patel.in</span>
+      </a>
+      <div>
+        <p>
+          This Website was made with <img src={reactIcon} alt="React" />
+          {/* <span>❤️</span> */}
+        </p>
+      </div>
+      <div className="social-media">
+        <a
+          href="https://www.linkedin.com/in/dhruvil-patel-7196021b4/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={linkedin} alt="Linkedin" />
+        </a>
+        <a
+          href="https://github.com/dhruvil1808"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={githubIcon} alt="GitHub" />
+        </a>
+        <a
+          href="https://api.whatsapp.com/send/?phone=%2B917045963959&text=Hello+Dhruvil"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={whatsapp} alt="Whatsapp" />
+        </a>
+        <a
+          href="https://www.instagram.com/dhruvil1808"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={instagramIcon} alt="Instagram" />
+        </a>
+      </div>
+    </Container>
+  )
+}
