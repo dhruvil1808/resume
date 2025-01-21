29b0a599a93ec49aa809eb554d83591ef5c7c204
Dhruvil Patel <dhruvilpatel2002@gmail.com>
2025-01-21 19:18:16 +0530

    some updates

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -19,17 +19,16 @@ export function About() {
         </ScrollAnimation>
         <ScrollAnimation animateIn="fadeInLeft" delay={0.1 * 1000}>
           <p>
-            Hi there! I'm Dhruvil, I'm a skilled automation and backend development professional with a track record of innovative solutions. Currently, I'm an Automation Intern at UBS (Union Bank of Switzerland), where I've created Python-based automation that saves the Valuations Department over 50 hours a month. At CollabNation, I was key to backend development, improving user experience through robust features and integrated analytics.
-          </p>
+          I'm a passionate Software Development Engineer specializing in automation and backend development. Currently, as a Valuations Developer/Controller at UBS (Union Bank of Switzerland), I design and implement Python-based solutions to streamline financial operations. My work directly contributes to improving process efficiency and accuracy, saving the Valuations Department over 50 hours per month. I focus on building scalable and high-performance systems that meet evolving business needs. Prior to UBS, I worked at CollabNation, where I contributed to backend architecture and development, enhancing user experience through the integration of robust features, APIs, and data analytics. My expertise in Python, software development, and automation drives me to build optimized, reliable, and maintainable systems that solve complex challenges.          </p>
         </ScrollAnimation>
         <ScrollAnimation animateIn="fadeInLeft" delay={0.2 * 1000} style={{ marginTop: "2rem", marginBottom: "2rem" }}>
           <p>
-          My personal projects reflect my passion for technology and problem-solving. I've developed a predictive text generator and a farmers' market platform, both highlighting my technical capabilities. I'm proficient in several programming languages and frameworks, with a strong interest in full-stack development, data science, and automation.
+          My personal projects showcase my software development skills and problem-solving abilities. I’ve developed applications like Chat App, Atendo, and Farmer Marketplace, each highlighting my expertise in building scalable systems and solving real-world challenges. I’m proficient in various programming languages and frameworks, with a focus on full-stack development, automation, and data-driven solutions.
           </p>
         </ScrollAnimation>
         <ScrollAnimation animateIn="fadeInLeft" delay={0.3 * 1000}>
           <p>
-          As a team player, I value communication and collaboration. My experience with supervisory platforms and data acquisition has further enhanced these skills. I believe my diverse background and collaborative approach make me a valuable asset to any team or organization.
+          I thrive in collaborative environments where clear communication and teamwork drive success. My experience with supervisory platforms and data acquisition has strengthened my ability to work effectively with cross-functional teams, ensuring seamless integration and problem-solving. I am confident that my technical skills, combined with my collaborative mindset, make me a valuable asset to any development team or organization.
           </p>
         </ScrollAnimation>
         <ScrollAnimation animateIn="fadeInLeft" delay={0.4 * 1000}>
--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -36,6 +36,33 @@ export function Project() {
             </footer>
           </div>
         </ScrollAnimation>
+        
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
+              <div className="project-links">
+                <a href="https://ping-n2g9.onrender.com/" target="_blank" rel="noreferrer">
+                  <img src={externalLink} alt="Visit site" />
+                </a>
+              </div>
+            </header>
+            <div className="body">
+              <h3>Ping - Real Time Chat Application</h3>
+              <p>
+              A real-time chat application that supports text messaging, File Sharing and video calling features. Users can create Groups, send messages, and initiate video calls with Friends
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>NodeJS</li>
+                <li>Socket.io</li>
+                <li>React JS</li>
+                <li>WebRTC</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation> 
 
         <ScrollAnimation animateIn="flipInX">
           <div className="project">
@@ -140,34 +167,7 @@ export function Project() {
               </ul>
             </footer>
           </div>
-        </ScrollAnimation>
-
-        <ScrollAnimation animateIn="flipInX">
-          <div className="project">
-            <header>
-              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
-              <div className="project-links">
-                <a href="https://github.com/dhruvil1808/ChatApp" target="_blank" rel="noreferrer">
-                  <img src={externalLink} alt="Visit site" />
-                </a>
-              </div>
-            </header>
-            <div className="body">
-              <h3>Chat App  - Real Time Chat Application</h3>
-              <p>
-              A real time chat application that allows users to chat with each other.
-              </p>
-            </div>
-            <footer>
-              <ul className="tech-list">
-                <li>NodeJS</li>
-                <li>Socket.io</li>
-                <li>Express</li>
-                <li>EJS</li>
-              </ul>
-            </footer>
-          </div>
-        </ScrollAnimation>       
+        </ScrollAnimation>      
 
       </div>
     </Container>
